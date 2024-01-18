d8a40fde7ce445de4c60e936ef0dbf9f03c28f58
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2024-01-18 11:28:14 +0700

    .

--- api.ts
+++ api.ts
@@ -6,8 +6,14 @@ import F01SARBALANCETABLE from "./flow/001/01SARBALANCETABLE";
 import F02SARBALANCECW from "./flow/001/02SARBALANCECW";
 import F03SARBALANCEICP from "./flow/001/03SARBALANCEICP";
 import F04SARBALANCESLUDGE from "./flow/001/04SARBALANCESLUDGE";
+import P1F04SARPHSTD from "./flow/001/20SARPHSTD";
+import P1F04SARFFSTD from "./flow/001/26SARFFSTD";
+
+import P2F01GETLIST from "./flow/002/01GETLIST";
 import testflow from "./flow/testflow/testflow";
 
+
+//26SARFFSTD
 // import flowloginlogin from "./flow/login/login";
 // import flowtestflowtestflow from "./flow/testflow/testflow";
 
@@ -18,6 +24,9 @@ router.use(F01SARBALANCETABLE);
 router.use(F02SARBALANCECW);
 router.use(F03SARBALANCEICP);
 router.use(F04SARBALANCESLUDGE);
+router.use(P1F04SARPHSTD);
+router.use(P2F01GETLIST);
+router.use(P1F04SARFFSTD);
 router.use(testflow);
 // router.use(flow004flow004);
 // router.use(flow005flow005);
---
+++ flow/002/01GETLIST.ts
@@ -0,0 +1,79 @@
+import express from "express";
+import { Router } from "express";
+import { mssqlquery } from "../../function/mssql";
+import { mongodbinsertMany, mongodbfind, mongodbfindsome, mongodbupdate } from "../../function/mongodb";
+
+const router: Router = express.Router();
+
+//`SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE  ItemStatus='LIST NORMAL' and (InstrumentName ='F-F' or InstrumentName ='pH' or InstrumentName = 'Cl(AUTO)') order by ID desc`
+
+
+router.post('/GETLIST/request_PH_ALL', async (req, res) => {
+  //-------------------------------------
+  console.log(req.body);
+  let input = req.body
+  //-------------------------------------
+  let output: any = []
+  if(input['name'] != undefined){
+
+    // console.log(mssql.qurey())
+    // let query = `SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE  ItemStatus='LIST NORMAL' and (InstrumentName ='pH') order by ID desc`
+    let query = `SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE    (InstrumentName ='pH') order by ID desc`
+    var findDB: any = await mssqlquery(query);
+    let data: any = findDB['recordsets'][0];
+    output = data;
+  }
+
+
+
+  //-------------------------------------
+  return res.json(output);
+});
+
+router.post('/GETLIST/request_FF_ALL', async (req, res) => {
+  //-------------------------------------
+  console.log(req.body);
+  let input = req.body
+  //-------------------------------------
+  let output: any = []
+  if(input['name'] != undefined){
+
+    // console.log(mssql.qurey())
+    // let query = `SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE  ItemStatus='LIST NORMAL' and (InstrumentName ='F-F') order by ID desc`
+    let query = `SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE    (InstrumentName ='F-F') order by ID desc`
+    var findDB: any = await mssqlquery(query);
+    let data: any = findDB['recordsets'][0];
+    output = data;
+  }
+
+
+
+  //-------------------------------------
+  return res.json(output);
+});
+
+router.post('/GETLIST/request_PH', async (req, res) => {
+  //-------------------------------------
+  console.log(req.body);
+  let input = req.body
+  //-------------------------------------
+  let output: any = []
+  if(input['name'] != undefined){
+
+    // console.log(mssql.qurey())
+    let query = `SELECT * FROM [SAR].[dbo].[Routine_RequestLab]  WHERE  ItemStatus='LIST NORMAL' and (InstrumentName ='pH') and [UserReceive]='${input['name']}' order by ID desc`
+    var findDB: any = await mssqlquery(query);
+    let data: any = findDB['recordsets'][0];
+    output = data;
+  }
+
+  res.json(output);
+
+
+
+  //-------------------------------------
+  res.json(output);
+});
+
+
+export default router;
